041a6cbf8ed40efae05808e587e681bcd5e04f66
rafael.calsaverini@gmail.com <ubuntu@ip-10-248-15-179.sa-east-1.compute.internal>
2012-12-24 20:35:26 +0000

    added tests, edge manipulation functions

--- Agent/Agent.py
+++ Agent/Agent.py
@@ -1,11 +1,32 @@
 import igraph
+import random
+
 class Agent(object):
-    def __init__(self, n = 10):
-        self.graph = igraph.Graph.Erdos_Renyi(n=n, p=0.5)
+    def __init__(self, n = 10, p=0.5):
+        self.graph = igraph.Graph.Erdos_Renyi(n=n, p=p)
+
+    def groupSize(self):
+        return self.graph.vcount()
 
     def pluck(self):
         (i,j) = self.getRandomVertexPair()
         self.pluckEdge(i,j)
+
+    def getRandomVertexPair(self):
+        n = self.graph.vcount()
+        return random.sample(xrange(n),2)
+
+    def pluckEdge(self, i,j):
+        try:
+            self.removeEdge(i,j)
+        except igraph.InternalError:
+            self.addEdge(i,j)
+
+    def removeEdge(self, i, j):
+        self.graph.delete_edges([(i,j)])
+
+    def addEdge(self, i, j):
+        self.graph.add_edge(i, j)
 
     def __str__(self):
         return str(self.graph)
--- tests/testAgent.py
+++ tests/testAgent.py
@@ -2,9 +2,29 @@ import unittest
 from Agent import Agent
 
 class testAgent(unittest.TestCase):
+    def setUp(self):
+        self.agent = Agent()
     def testPluck(self):
-        agent = Agent()
-        agent.pluck()
+        self.agent.pluck()
 
+    def testGroupSize(self):
+        agent = Agent(n=10)
+        self.assertEqual(agent.groupSize(), 10)
 
+    def testRandomVertexPair(self):
+        (i,j) = self.agent.getRandomVertexPair()
+        self.assertGreater(self.agent.groupSize(), i)
+        self.assertGreater(self.agent.groupSize(), j)
+
+    def testAddAndRemoveEdge(self):
+        agent = Agent(n=10, p = 0)
+        self.assertFalse(agent.graph.are_connected(0,1))
+        agent.addEdge(0,1)
+        self.assertTrue(agent.graph.are_connected(0,1))
+        agent.removeEdge(0,1)
+        self.assertFalse(agent.graph.are_connected(0,1))
+
+    def testPluckEdge(self):
+        agent = Agent(n=10, p =0)
+        agent.pluckEdge(1,2)
 
