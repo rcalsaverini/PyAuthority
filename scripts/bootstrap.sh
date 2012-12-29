d036f905a77708da937395a2f03dbfe82d4fddd8
Rafael S. Calsaverini <rafael.calsaverini@gmail.com>
2012-12-29 12:37:35 -0200

    boostrap fixed again :(

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo aptitude update
-sudo aptitude install build-essential libxml2-dev libglpk-dev libgmp3-dev libblas-dev liblapack-dev libarpack2-dev python-dev python-numpy python-pip git
+sudo aptitude install build-essential libxml2-dev libglpk-dev libgmp3-dev libblas-dev liblapack-dev libarpack2-dev python-dev python-numpy python-pip git -y
 wget http://dl.dropbox.com/u/875394/igraph.tgz
 tar -xvzf igraph.tgz
 
@@ -8,7 +8,8 @@ cd igraph-0.6
 ./configure
 make
 sudo make install
-
+sudo echo "/usr/local/lib" >> /etc/ld.so.conf
+sudo ldconfig
 cd ../python-igraph-0.6
 sudo python setup.py install
 
