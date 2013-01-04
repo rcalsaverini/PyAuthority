131706614e2ea23fa52006d8ff5708c4c7758ab7
Rafael S. Calsaverini <rafael.calsaverini@gmail.com>
2013-01-04 11:11:33 -0200

    ok

--- scripts/generateCSV.py
+++ scripts/generateCSV.py
@@ -6,6 +6,7 @@ nsteps = 1000000
 burnin = 100000
 thin = 3
 beta = 7.0
-for n in range(10, 21):
-    for alpha in linspace(0, 2.0, 101):
-        print n, alpha, beta, nsteps, burnin, thin
+for beta in linspace(0.01, 10.0, 21):
+    for n in [10, 12, 14, 16, 18, 20]:
+        for alpha in linspace(0, 2.0, 101):
+            print n, alpha, beta, nsteps, burnin, thin
--- scripts/run.sql
+++ scripts/run.sql
@@ -1,14 +1,20 @@
-CREATE EXTERNAL TABLE parameters
-    (numAgents INT,
-     alpha DOUBLE,
-     beta DOUBLE,
-     nsteps INT,
-     burnin INT,
-     thin INT)
-    ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LINES TERMINATED BY '\n'
-    STORED AS TEXTFILE LOCATION 's3://doutorado/parameters/';
+ADD JAR /home/hadoo/hive/contrib/hive-contrib-0.8.1.jar
 
-CREATE EXTERNAL TABLE results
+CREATE EXTERNAL TABLE rawResults
+    (numAgents STRING,
+     alpha STRING,
+     beta STRING,
+     energy STRING,
+     avgDegree STRING,
+     maxDegree STRING,
+     accept STRING)
+    ROW FORMAT SERDE 'org.apache.hadoop.hive.contrib.serde2.RegexSerDe'
+    WITH SERDEPROPERTIES(
+        "input.regex" = "([\d|\.]+)\s+([\d|\.]+)\s+([\d|\.]+)\s+([\d|\.]+)\s+([\d|\.]+)\s+([\d|\.]+)\s+([\d|\.]+)",
+        "output.format.string" = "%1$i %2$f %3$f %4$f %5$f %6$f")
+    STORED AS TEXTFILE LOCATION 's3://doutorado/results/';
+
+CREATE EXTERNAL TABLE avgResults
     (numAgents INT,
      alpha DOUBLE,
      beta DOUBLE,
@@ -16,26 +22,10 @@ CREATE EXTERNAL TABLE results
      avgDegree DOUBLE,
      maxDegree DOUBLE,
      accept DOUBLE)
-    ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LINES TERMINATED BY '\n'
-    STORED AS TEXTFILE LOCATION 's3://doutorado/results/'
+    ROW FORMAT DELIMITED FIELDS TERMINATED BY ' \t' LINES TERMINATED BY '\n'
+    STORED AS TEXTFILE LOCATION 's3://doutorado/avgResults/';
 
-
-
-FROM (
-    MAP
-        burnin, nsteps
-    USING
-        'echo'
-    FROM
-        parameters) map_out SELECT * ;
-
-FROM
-    (MAP
-        numAgents, alpha, beta, burnin, nsteps, thin
-     USING
-        'runAuthorityMCMC' AS numAgents, alpha, beta, energy, avgDegree, maxDegree, accept
-     FROM
-        parameters
-    ) map_output
-INSERT OVERWRITE TABLE parameters SELECT *;
+INSERT OVERWRITE TABLE avgResults
+SELECT numAgents, alpha, beta, avg(energy), avg(avgDegree), avg(maxDegree), avg(accept) FROM rawResults
+GROUP BY numAgents, alpha, beta;
 
