03181ae82efe901821c572112da0f51480874a31
Rafael S. Calsaverini <rafael.calsaverini@gmail.com>
2013-01-09 10:58:42 -0200

    prepared

--- scripts/generateCSV.py
+++ scripts/generateCSV.py
@@ -5,8 +5,6 @@ n = 15
 nsteps = 1000000
 burnin = 100000
 thin = 3
-beta = 7.0
-for beta in linspace(0.01, 10.0, 21):
-    for n in [10, 12, 14, 16, 18, 20]:
-        for alpha in linspace(0, 2.0, 101):
-            print n, alpha, beta, nsteps, burnin, thin
+beta = 10.0
+for alpha in linspace(0, 2.0, 101):
+    print n, alpha, beta, nsteps, burnin, thin
